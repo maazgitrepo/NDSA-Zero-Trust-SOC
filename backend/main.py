04d76a76015df4bfe14c16152e0a2a6bb2831b88
Maaz Farrukh <maaz.farrukh3@gmail.com>
2026-07-28 14:04:29 +0500

    Add health check API

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
+from api.routes import router
+
 app = FastAPI(title="NDSA Zero Trust SOC")
+
+app.include_router(router)
+
 @app.get("/")
 def root():
     return {"message": "NDSA Zero Trust SOC API is running"}
